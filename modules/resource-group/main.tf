9823dc9bb5f3e713d98253c1f6244c1d8afe4865
Jayant Jawa <jawa.jayant.jj@gmail.com>
2026-04-19 09:33:07 +1000

    feat: add resource-group module main.tf

---
+++ modules/resource-group/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0"
+    }
+  }
+}
+
+resource "azurerm_resource_group" "this" {
+  name     = var.name
+  location = var.location
+  tags     = var.tags
+}
