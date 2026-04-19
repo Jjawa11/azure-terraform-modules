c7f50f0eb4e913a2eb8fad51d878375aafd991f7
Jayant Jawa <jawa.jayant.jj@gmail.com>
2026-04-19 10:07:24 +1000

    feat: add log-analytics module main.tf

---
+++ modules/log-analytics/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0"
+    }
+  }
+}
+
+resource "azurerm_log_analytics_workspace" "this" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = var.sku
+  retention_in_days   = var.retention_in_days
+  tags                = var.tags
+}
+
+resource "azurerm_log_analytics_solution" "this" {
+  for_each = { for s in var.solutions : s.name => s }
+
+  solution_name         = each.value.name
+  location              = var.location
+  resource_group_name   = var.resource_group_name
+  workspace_resource_id = azurerm_log_analytics_workspace.this.id
+  workspace_name        = azurerm_log_analytics_workspace.this.name
+
+  plan {
+    publisher = each.value.publisher
+    product   = each.value.product
+  }
+}
