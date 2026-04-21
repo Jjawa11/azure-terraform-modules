1a3fe7864ebd32da23af7ce84d994a2bd8295487
Jayant Jawa <jawa.jayant.jj@gmail.com>
2026-04-21 12:47:07 +1000

    Create main.tffeat: add virtual-machine module main.tf with Linux and Windows support

---
+++ modules/virtual-machine/main.tf
@@ -0,0 +1,92 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0"
+    }
+  }
+}
+
+resource "azurerm_network_interface" "this" {
+  name                = "${var.name}-nic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  tags                = var.tags
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = var.create_public_ip ? azurerm_public_ip.this[0].id : null
+  }
+}
+
+resource "azurerm_public_ip" "this" {
+  count               = var.create_public_ip ? 1 : 0
+  name                = "${var.name}-pip"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  tags                = var.tags
+}
+
+resource "azurerm_linux_virtual_machine" "this" {
+  count               = var.os_type == "linux" ? 1 : 0
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  size                = var.vm_size
+  admin_username      = var.admin_username
+  tags                = var.tags
+
+  network_interface_ids = [azurerm_network_interface.this.id]
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = var.admin_ssh_public_key
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = var.os_disk_type
+    disk_size_gb         = var.os_disk_size_gb
+  }
+
+  source_image_reference {
+    publisher = var.image_publisher
+    offer     = var.image_offer
+    sku       = var.image_sku
+    version   = var.image_version
+  }
+
+  boot_diagnostics {}
+}
+
+resource "azurerm_windows_virtual_machine" "this" {
+  count               = var.os_type == "windows" ? 1 : 0
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  size                = var.vm_size
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  tags                = var.tags
+
+  network_interface_ids = [azurerm_network_interface.this.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = var.os_disk_type
+    disk_size_gb         = var.os_disk_size_gb
+  }
+
+  source_image_reference {
+    publisher = var.image_publisher
+    offer     = var.image_offer
+    sku       = var.image_sku
+    version   = var.image_version
+  }
+
+  boot_diagnostics {}
+}
